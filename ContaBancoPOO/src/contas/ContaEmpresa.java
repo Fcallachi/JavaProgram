5f9dad13118dbc80fec9e0226d8ad4665a13935c
unknown <fernandocallachi@gmail.com>
2020-09-18 12:58:19 -0300

    versao1

---
+++ ContaBancoPOO/src/contas/ContaEmpresa.java
@@ -0,0 +1,24 @@
+package contas;
+
+public class ContaEmpresa extends ContaBancaria{
+
+	private double limiteEmpresa;
+	
+	
+	//metodos
+	public void emprestimoEmpresarial() {
+		
+	}
+	
+	//get and set
+	public double getLimiteEmpresa() {
+		return limiteEmpresa;
+	}
+
+	public void setLimiteEmpresa(double limiteEmpresa) {
+		this.limiteEmpresa = limiteEmpresa;
+	}
+	
+	
+	
+}
